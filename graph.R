7a69346112cbfbced25a352e841580039aa98528
Guillem Miró Almuni <guillem@MacBook-Air-de-Guillem.local>
2021-01-28 17:32:29 +0100

    correción del gráfico

--- graph.R
+++ graph.R
@@ -3,5 +3,5 @@ dat[is.na(dat)] <- 0
 dat <- dat[, 25:64]
 values <- colMeans(dat)
 years <- c(1980:2019)
-plot(years, values, xlab="AÑOS", ylab="PORCENTAJE DE IMMUNIZADOS (%)", main= "IMMUNIZACIÓN CONTRA EL SARAMPIÓN DE NIÑOS ENTRE 12 y 23 MESES DE EDAD", ylim=c(0,100), pch=4, cex=.6, col="black")
+plot(years, values, xlab="AÑOS", ylab="PORCENTAJE DE INMUNIZADOS (%)", main= "INMUNIZACIÓN CONTRA EL SARAMPIÓN DE NIÑOS ENTRE 12 y 23 MESES DE EDAD", ylim=c(0,100), pch=4, cex=.6, col="black")
 lines(years,values, col="blue")
